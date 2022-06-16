595f619ceed4f839a55d842ee345e3704a8856c4
dekapd99 <dekaprimatio@gmail.com>
2022-06-16 20:08:20 +0700

    Initial Commit

---
+++ Tasker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tasker
+//
+//  Created by Deka Primatio on 16/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by Deka Primatio on 16/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
